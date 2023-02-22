9e211b1a1e6eba65bedf53a46cefc6497a16efd1
Mieu <mieuhamchoi@gmail.com>
2023-02-22 20:45:20 +0700

    connected mysql

--- overviewDjangoServer/settings.py
+++ overviewDjangoServer/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'overviewDjangoServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ reame.md
@@ -0,0 +1,9 @@
+venv\Scripts\activate
+
+pip install django
+
+django-admin startproject mysite
+
+python manage.py runserver
+
+python manage.py migrate
